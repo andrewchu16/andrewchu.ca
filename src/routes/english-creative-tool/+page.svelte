edbe31f8263b10d9df93c54b4e6c472b42cd1097
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2024-06-12 22:20:58 -0400

    finished phone appearance

--- src/routes/english-creative-tool/+page.svelte
+++ src/routes/english-creative-tool/+page.svelte
@@ -3,30 +3,45 @@
 <svelte:head>
     <title>English Creative Tool</title>
 </svelte:head>
-<div class="bg-neutral-800 w-screen h-screen flex justify-center items-center z-0">
-    <div class="relative">
-        <!-- Ringer Toggle Button -->
-        <div class="absolute -z-2 h-[20px] w-[6px] bg-neutral-100 -left-[6px] top-[20%]" />
-        <!-- Volume Buttons -->
-        <div class="absolute z-0 h-[100px] w-[6px] -left-[6px] top-[27%] flex flex-col justify-between">
-            <div class="h-[40px] bg-neutral-400 border-l-[1px] border-t-[1px] border-b-[1px] border-l-neutral-300 border-y-neutral-500 rounded-md"></div>
-            <div class="h-[40px] bg-neutral-400 border-l-[1px] border-t-[1px] border-b-[1px] border-l-neutral-300 border-y-neutral-500 rounded-md"></div>
-        </div>
-        <!-- Power Button -->
-        <!-- Body -->
+<!-- Phone Container -->
+<div class="relative">
+    <!-- Notch  -->
+    <div class="absolute top-0 left-[27%] bg-neutral-900 rounded-b-2xl w-[46%] h-[35px]">
+
+    </div>
+    <!-- Ringer Toggle Button -->
+    <div
+        class="absolute h-[3.5%] w-[6px] -left-[6px] top-[13%] bg-neutral-400 border-l-[1px] border-y-[1px] border-l-neutral-300 border-y-neutral-500 rounded-md"
+    />
+    <!-- Volume Buttons -->
+    <div class="absolute h-[15%] w-[6px] -left-[6px] top-[20%] flex flex-col justify-between z-10">
         <div
-            class="border-neutral-900 border-[11px] aspect-screen h-[90vh] rounded-[50px] outline-neutral-400 outline-[4px] outline bg-neutral-300 grid grid-rows-2 grid-cols-2 overflow-clip z-10 relative"
-        >
-            <div class="bg-red-200" />
-            <div class="bg-blue-200" />
-            <div class="bg-blue-200" />
-            <div class="bg-red-200" />
-        </div>
+            class="h-[40%] bg-neutral-400 border-l-[1px] border-y-[1px] border-l-neutral-300 border-y-neutral-500 rounded-md"
+        />
+        <div
+            class="h-[40%] bg-neutral-400 border-l-[1px] border-y-[1px] border-l-neutral-300 border-y-neutral-500 rounded-md"
+        />
+    </div>
+    <!-- Power Button -->
+    <div
+        class="absolute h-[10%] w-[6px] -right-[6px] top-[22.5%] bg-neutral-400 border-r-[1px] border-y-[1px] border-y-neutral-500 border-r-neutral-500 rounded-md"
+    />
+    <!-- Screen -->
+    <div
+        class="grid grid-rows-2 grid-cols-2 w-full aspect-screen h-[90vh] border-neutral-900 border-[11px] rounded-[6vh] outline-neutral-400 outline-[4px] outline overflow-clip"
+    >
+        <div class="bg-red-200" />
+        <div class="bg-blue-200" />
+        <div class="bg-blue-200" />
+        <div class="bg-red-200" />
     </div>
 </div>
 
 <style>
     .aspect-screen {
-        aspect-ratio: 9 / 17;
+        aspect-ratio: 9 / 18;
+    }
+    :global(body) {
+        @apply bg-neutral-800 w-screen h-screen flex justify-center items-center;
     }
 </style>
