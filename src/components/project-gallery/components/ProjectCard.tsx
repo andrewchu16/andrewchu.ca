f206f9db4cab4e8ed1881e7c03330493cc5ab0f0
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2025-02-02 00:24:30 -0500

    Update text selection styling

--- src/components/project-gallery/components/ProjectCard.tsx
+++ src/components/project-gallery/components/ProjectCard.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function ProjectCard({ project }: { project: Project }) {
   return (
-    <div className="rounded-lg flex flex-col lg:min-w-72 lg:max-w-72 w-full max-w-84 dark:border-0 dark:shadow-sm dark:shadow-neutral-700 border dark:border-secondary gap-2">
+    <div className="rounded-lg flex flex-col lg:min-w-72 lg:max-w-72 w-full max-w-84 dark:border-0 dark:shadow-sm dark:shadow-neutral-700 border dark:border-secondary gap-2 dark:selection:bg-neutral-700">
       <div className="relative w-full h-32 rounded-t-md overflow-hidden">
         <Image
           src={project.image}
