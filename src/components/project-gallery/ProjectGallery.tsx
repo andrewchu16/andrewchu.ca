e585f889abb252a07b647a302ee91858edf7ffbd
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2025-02-03 11:46:22 -0500

    Add github and devpost link to projects

--- src/components/project-gallery/ProjectGallery.tsx
+++ src/components/project-gallery/ProjectGallery.tsx
@@ -1,6 +1,7 @@
 import projects from "@/data/projects";
 import ProjectCard from "./components/ProjectCard";
 import { useEffect, useState } from "react";
+import WebLink from "../utils/WebLink";
 
 interface ProjectGalleryProps {
   searchQuery: string;
@@ -37,6 +38,16 @@ export default function ProjectGallery({
       {filteredProjects.map((project) => (
         <ProjectCard project={project} key={project.name} />
       ))}
+      <div className="text-sm sm:text-base rounded-lg w-screen lg:w-72 max-w-full h-full min-h-40 flex items-center justify-center flex-col text-body border border-dashed border-neutral-400 dark:border-neutral-700">
+        <p className="text-center max-w-52">
+          View the rest of my projects on{" "}
+          <WebLink href="https://github.com/andrewchu16">GitHub</WebLink> and{" "}
+          <WebLink href="https://devpost.com/Previouslynamedjeff">
+            Devpost
+          </WebLink>
+          .
+        </p>
+      </div>
       {filteredProjects.length === 0 && (
         <>
           <div className="w-screen lg:w-72 max-w-full h-60"></div>
